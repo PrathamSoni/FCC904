ef0ff36526714ca47aa4554c8765dca190fddda9
Pratham Soni <psoni@DN0a1f0a70.SUNet>
2022-04-22 01:41:49 -0700

    correct shape version

--- data/get_shapes.sh
+++ data/get_shapes.sh
@@ -1,13 +1,15 @@
 for i in {1..9}; do
-  wget https://www2.census.gov/geo/tiger/TIGER2020PL/LAYER/BG/2020/tl_2020_0${i}_bg20.zip &
+  wget https://www2.census.gov/geo/tiger/TIGER2019/BG/tl_2019_0${i}_bg.zip &
 done
 
 for i in {10..50}; do
-  wget https://www2.census.gov/geo/tiger/TIGER2020PL/LAYER/BG/2020/tl_2020_${i}_bg20.zip &
+  wget https://www2.census.gov/geo/tiger/TIGER2019/BG/tl_2019_${i}_bg.zip &
 done
 
 for i in 51 53 54 55 56 72; do
-  wget https://www2.census.gov/geo/tiger/TIGER2020PL/LAYER/BG/2020/tl_2020_${i}_bg20.zip &
+  wget https://www2.census.gov/geo/tiger/TIGER2019/BG/tl_2019_${i}_bg.zip &
 done
 
+wait
 unzip '*.zip' -d all_data
+rm -f *.zip
